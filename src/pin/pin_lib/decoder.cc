332594e12f0baf8c4192a3f90c745546cf99fb5f
aaryatopiwala <75400069+aaryatopiwala@users.noreply.github.com>
2024-02-13 20:00:36 -0800

    Update decoder.cc

--- src/pin/pin_lib/decoder.cc
+++ src/pin/pin_lib/decoder.cc
@@ -137,7 +137,7 @@ ctype_pin_inst* pin_decoder_get_latest_inst() {
 }
 
 void pin_decoder_print_unknown_opcodes() {
-  for(const auto opcode : unknown_opcodes) {
+  for(const auto &opcode : unknown_opcodes) { // ampersand added to prevent an error occuring during make
     (*glb_err_ostream) << opcode << std::endl;
   }
 }
